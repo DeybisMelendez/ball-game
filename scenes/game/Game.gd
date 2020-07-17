42d066918884c1cc35e1841a29bc3f899ed9623c
DeybisMelendez <pixelweb2007@gmail.com>
2020-07-17 10:40:21 -0600

    reorganización general

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -1,21 +1,32 @@
 extends Node2D
 
 export (PackedScene) var Ball
+
+onready var Press = $Main/MainMenu/Press
+onready var LoseMenu = $Main/LoseMenu
+onready var Max = $Main/Max
+onready var Leaderboard = $Main/MainMenu/Leaderboard
+onready var PlayerBall = $PlayerBall
+onready var Title = $Main/MainMenu/Title
+onready var Desc = $Main/MainMenu/Desc
+onready var Score = $Main/Score
+onready var Audio = $Main/Audio
+
 var score = 0
 func _ready():
-	$CanvasLayer/Press.connect("button_up",self,"start_game")
-	$CanvasLayer/Ups.connect("button_up",self,"restart")
-	$CanvasLayer/Max.text = "Max:" + str(Global.max_record)
-	$CanvasLayer/Leaderboard.connect("button_up", self, "leaderboard")
+	Press.connect("button_up",self,"start_game")
+	LoseMenu.get_node("PlayAgain").connect("button_up",self,"restart")
+	Max.text = "Max:" + str(Global.max_record)
+	Leaderboard.connect("button_up", self, "leaderboard")
 	randomize()
 
 func start_game():
-	$PlayerBall.can_move = true
-	$CanvasLayer/Title.hide()
-	$CanvasLayer/Press.hide()
-	$CanvasLayer/Leaderboard.hide()
-	$CanvasLayer/Title3.hide()
-	$CanvasLayer/Score.show()
+	PlayerBall.can_move = true
+	Title.hide()
+	Press.hide()
+	Leaderboard.hide()
+	Desc.hide()
+	Score.show()
 	for i in 24:
 		instance_ball(false)
 
@@ -29,21 +40,21 @@ func end():
 		Global.max_record = score
 		Global.save_data(Global.max_record, "data.dat")
 		get_tree().paused = false
-		$CanvasLayer/AudioStreamPlayer.play()
-		yield($CanvasLayer/AudioStreamPlayer,"finished")
+		Audio.play()
+		yield(Audio,"finished")
 		get_tree().change_scene("res://scenes/new_record/NewRecord.tscn")
 	else:
-		$CanvasLayer/AudioStreamPlayer.play()
-		$CanvasLayer/Ups.show()
+		Audio.play()
+		LoseMenu.show()
 
 func instance_ball(add_score):
 	var new = Ball.instance()
-	new.scale_size = $PlayerBall.scale_size * rand_range(0.2,1.5)
+	new.scale_size = PlayerBall.scale_size * rand_range(0.2,1.5)
 	new.position = Vector2((randi()%2)*720, (randi()%2)*1280)
 	call_deferred("add_child", new)
 	if add_score:
 		score += 1
-		$CanvasLayer/Score.text = str(score)
+		Score.text = str(score)
 
 func leaderboard():
 	get_tree().change_scene("res://addons/silent_wolf/Scores/Leaderboard.tscn")
